66c5820bc316dacc10c389e6e9312c420c464980
Vivan Shah <vivanshah@gmail.com>
2016-12-08 21:05:30 -0500

    remove old code from d8

--- day8.cs
+++ day8.cs
@@ -73,21 +73,20 @@ namespace AdventOfCode2016
                 switch (instructions.First())
                 {
                     case "rect":
-                        rect(screen, Int32.Parse(instructions[1]), Int32.Parse(instructions[2]));
+                        rect(screen, int.Parse(instructions[1]), int.Parse(instructions[2]));
                         break;
                     case "rotate":
                         if(instructions[1] == "row")
                         {
-                            rotateRow(screen, Int32.Parse(instructions[3]), Int32.Parse(instructions[4]));
+                            rotateRow(screen, int.Parse(instructions[3]), int.Parse(instructions[4]));
                         }
                         else
                         {
-                            rotateColumn(screen, Int32.Parse(instructions[2]), Int32.Parse(instructions[3]));
+                            rotateColumn(screen, int.Parse(instructions[2]), int.Parse(instructions[3]));
                         }
                         break;
                 }
-
-                PrintScreen(screen);
+                //PrintScreen(screen);
             }
             Console.WriteLine(PrintScreen(screen) + " lit pixels");
             st.Stop();
@@ -110,131 +109,6 @@ namespace AdventOfCode2016
                 Console.Write(Environment.NewLine);
             }
             return litPixels;
-        }
-        public void calculate2()
-        {
-            var st = new Stopwatch();
-            st.Start();
-            IEnumerable<string> input = File.ReadAllLines("input\\day7.txt");
-            int sslIPs = 0;
-            Parallel.ForEach(input, line => 
-            {
-                char[] queue = new char[3];
-                int queuePtr = 0;
-                bool inHypernet = false;
-                var abaInHypernet = new HashSet<string>();
-                var abaInSupernet = new HashSet<string>();
-                string reverseAba = null;
-                foreach (var c in line)
-                {
-                    if (c == '[')
-                    {
-                        inHypernet = true;
-                        Array.Clear(queue, 0, 3);
-                        queuePtr = 0;
-                        continue;
-                    }
-                    else if (c == ']')
-                    {
-                        Array.Clear(queue, 0, 3);
-                        queuePtr = 0;
-                        inHypernet = false;
-                        continue;
-                    }
-                    else
-                    {
-                        Enqueue(c, queue, ref queuePtr, 3);
-                    }
-
-                    var aba = GetABA(queue, queuePtr);
-
-                    if ( aba != null)
-                    {
-                        reverseAba = ReverseABA(aba);
-                        if (inHypernet)
-                        {
-                            abaInHypernet.Add(aba);
-                            if (abaInSupernet.Contains(reverseAba))
-                            {
-                                Interlocked.Increment(ref sslIPs);
-                                break;
-                            }
-                            
-                        }
-                        else //in supernet
-                        {
-                            abaInSupernet.Add(aba);
-                            if (abaInHypernet.Contains(reverseAba))
-                            {
-                                Interlocked.Increment(ref sslIPs);
-                                break;
-                            }
-                            
-                        }
-                    }
-
-                }
-            });
-            st.Stop();
-            Console.WriteLine(sslIPs + " in " + st.Elapsed.TotalMilliseconds);
-        }
-
-        private static string ReverseABA(string aba)
-        {
-            return new string(new char[] {aba[1], aba[0], aba[1]});
-        }
-
-        private static void Enqueue(char c, char[] queue, ref int queuePtr, int maxSize)
-        {
-            queue[queuePtr] = c;
-            queuePtr++;
-            if (queuePtr == maxSize)
-            {
-                queuePtr = 0;
-            }
-        }
-
-        private static bool CheckAbba(char[] queue, int queuePtr)
-        {
-            switch(queuePtr)
-            {
-                case 0:
-                    return queue[0] != queue[1] && queue[1] == queue[2] && queue[0] == queue[3];
-                case 1:
-                    return queue[1] != queue[2] && queue[2] == queue[3] && queue[1] == queue[0];
-                case 2:
-                    return queue[2] != queue[3] && queue[3] == queue[0] && queue[2] == queue[1];
-                case 3:
-                    return queue[3] != queue[0] && queue[0] == queue[1] && queue[3] == queue[2];
-                default:
-                    return false;
-            }
-        }
-
-        private string GetABA( char[] queue, int queuePtr)
-        {
-            switch (queuePtr)
-            {
-                case 0:
-                    if( queue[0] != queue[1] && queue[0] == queue[2])
-                    {
-                        return new string(queue);
-                    }
-                    break;
-                case 1:
-                    if(queue[1] != queue[2] && queue[1] == queue[0])
-                    {
-                        return new string(new char[] {queue[1], queue[2], queue[0]});
-                    }
-                    break;
-                case 2:
-                    if (queue[2] != queue[0] && queue[2] == queue[1])
-                    {
-                        return new string(new char[] { queue[2], queue[0], queue[1] });
-                    }
-                    break;
-            }
-            return null;
         }
     }
 }
